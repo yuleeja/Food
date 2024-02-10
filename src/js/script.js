7defffa60f5e9d22a836a0c6cbec0433f9ba3a60
Yulia Mamaeva <yulia.mamaeva00@gmail.com>
2024-02-10 20:10:53 +0300

    Добавлен таймер обратного отсчёта (для акции)

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
 
+    //TABS
+
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -36,4 +38,71 @@ window.addEventListener('DOMContentLoaded', function () {
             });
         }
     });
+
+    //TIMER
+
+    const deadline = '2024-03-01';
+
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date()); //получим кол-во мс, которое осталось до deadline
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)), // когда общее кол-во мс / на кол-во мс в одних сутках, мы получим сколько суток у нас осталось до deadline, floor - округлит значение
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    //функция, которая добавляет 0 перед цифрами: 00~09
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            //запуск функции через 1с:
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); //вызываем эту функцию вручную, чтобы в таймере не было видно значений из верстки. Т.к. функция запускается через 1с.
+
+        //функция, которая будет обновлять таймер каждую секунду:
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval); //если время вышло, то таймер не будет обновляться
+            }
+
+        }
+
+    }
+    setClock('.timer', deadline)
 });
