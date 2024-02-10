61ccac58346c7d9b53a0d946e8f3f55121212357
Yulia Mamaeva <yulia.mamaeva00@gmail.com>
2024-02-10 08:52:06 +0300

    добавлено переключение табов

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,40 @@ var __webpack_exports__ = {};
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
+window.addEventListener('DOMContentLoaded', function () {
+  // Tabs
 
+  let tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+  function showTabContent(i) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  hideTabContent();
+  showTabContent(0);
+  tabsParent.addEventListener('click', function (event) {
+    const target = event.target;
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map
